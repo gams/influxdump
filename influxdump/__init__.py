8ebae45ae5c53899158bfeafe70d73d8a1ef6ac0
Stefan 'hr' Berder <hr@bonz.org>
2020-03-23 10:47:57 +0800

    bump version to 1.0.4

--- influxdump/__init__.py
+++ influxdump/__init__.py
@@ -11,7 +11,7 @@ from exception import TypecastError
 
 __author__ = 'Stefan Berder <stefan@measureofquality.com>'
 __contact__ = 'code+influxdump@measureofquality.com'
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 CHUNKSIZE = 50000
 
